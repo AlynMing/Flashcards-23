4c99e3f3be64e759f9e3162d2556f44e58a51505
yichenzhu <yichen.zhu@gmail.com>
2020-02-21 11:24:09 -0600

    Initial Commit

---
+++ Flashcard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flashcard
+//
+//  Created by Yichen Zhu on 2/21/20.
+//  Copyright © 2020 Yichen Zhu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
